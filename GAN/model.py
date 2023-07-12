017c6fd0d11f016152f513cbdfa54a4add95b4b4
TangJian <tang2602072248@126.com>
2023-07-12 22:22:35 +0800

    Add model file of GAN

---
+++ GAN/model.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/7/12 22:01
+# @Author  : Tang
+# @File    : model.py
+# @Software: PyCharm
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Deep-learning-models
+
+* GAN
