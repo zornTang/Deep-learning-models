1a6a704d17d58269cfc71fc8e5911dcb397944c2
TangJian <tang2602072248@126.com>
2023-07-12 21:47:06 +0800

    Modify test file to whether prime program

--- test.py
+++ test.py
@@ -3,4 +3,19 @@
 # @Author  : Tang
 # @File    : test.py
 # @Software: PyCharm
-print('Hello, world!')
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+# 示例用法
+num = int(input("请输入一个正整数: "))
+if is_prime(num):
+    print(num, "是质数")
+else:
+    print(num, "不是质数")
