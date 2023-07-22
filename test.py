e272075cedcb6f81d1b457c3537c70606cf6873d
zorn <aloyalfanv@gmail.com>
2023-07-22 23:25:48 +0800

    modify test.py file

--- test.py
+++ test.py
@@ -5,6 +5,29 @@
 #@Software: VScode
 
 
+# 判断数字是否为质数
+def is_prime(n):
+    """
+    判断数字是否为质数
+    :param n: 数字
+    :return: True or False
+    """
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+# 输入数字
+num = int(input("请输入一个数字："))
+
+# 调用函数
+if is_prime(num):
+    print("{}是质数".format(num))
+
+else:
+    print("{}不是质数".format(num))
 
 
 
